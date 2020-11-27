fb25448f1c15b4991d9f85f59b2a6213b56d6180
Moncef AKTOUF <moncef.aktouf.dz@viacesi.fr>
2020-11-27 23:16:04 +0100

    moncef 05

--- PROJECT/commande.h
+++ PROJECT/commande.h
@@ -2,24 +2,24 @@
 class Commande
 {
 private:
-	string date_livraison_commancde, date_emission_commancde, date_paiment_commancde, date_solde_commancde, moyen_paiment_commancde, nom_article_commancde, tab_article_n[50];
+	string date_livraison_commande, date_emission_commande, date_paiment_commande, date_solde_commande, moyen_paiment_commande, nom_article_commande, tab_article_n[50];
 	float montant_HT, montant_TVA, montant_TTC, remise;
 	int tab_article_qt[50], n_incre, quantite;
 public:
 	Commande();
-	Commande(string date_livraison_commancde, string date_emission_commancde, string date_paiment_commancde, string date_solde_commancde, string moyen_paiment_commancde, string nom_article_commancde, float montant_HT, float TVA, float montant_TTC, float remise, int quantite);
+	Commande(string date_livraison_commande, string date_emission_commande, string date_paiment_commande, string date_solde_commande, string moyen_paiment_commande, string nom_article_commande, float montant_HT, float TVA, float montant_TTC, float remise, int quantite);
 	string getdate_liv();
-	void setdate_liv(string date_livraison_commancde);
+	void setdate_liv(string date_livraison_commande);
 	string getdate_emission();
-	void setdate_emission(string date_emission_commancde);
+	void setdate_emission(string date_emission_commande);
 	string getdate_paiment();
-	void setdate_paiment(string date_paiment_commancde);
+	void setdate_paiment(string date_paiment_commande);
 	string getdate_solde();
-	void setdate_solde(string date_solde_commancde);
+	void setdate_solde(string date_solde_commande);
 	string getmoyen_paiment();
-	void setmoyen_paiment(string moyen_paiment_commancde);
+	void setmoyen_paiment(string moyen_paiment_commande);
 	string getnom_article();
-	void setnom_article(string nom_article_commancde);
+	void setnom_article(string nom_article_commande);
 	float getmontant_HT();
 	void setmontant_HT();
 	float getmontant_TVA();
